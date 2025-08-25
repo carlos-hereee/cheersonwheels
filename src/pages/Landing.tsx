15589179d52f3294c6c5e73efc0cbd8ae1342b9a
carlos-hereee <97hernandez.c@gmail.com>
2025-08-25 14:11:12 -0500

    init landing page

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,12 @@
+const Landing =()=> {
+
+  return (
+    <div className="app-body">
+
+ <h1>Welcome to Cheerson Wheels</h1>
+ <p>Your one-stop shop for all things Cheerson.</p>    
+    </div>
+  )
+}
+
+export default Landing
