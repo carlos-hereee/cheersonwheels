9f3e9531c9cbf0dc47689c2a47ff8dc764e42355
carlos-hereee <97hernandez.c@gmail.com>
2025-08-25 15:33:49 -0500

    add demo data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Header } from 'nexious-library';
+
 const App = () => {
   return (
     <div className="app-body">
-      <h1 className="heading">Welcome to Cheerson Wheels</h1>
-      <p>Your one-stop shop for all things Cheerson.</p>
+      <Header />
+      {/* <h1 className="heading">Welcome to Cheerson Wheels</h1>
+      <p>Your one-stop shop for all things Cheerson.</p> */}
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import { ScrollToTop } from 'nexious-library';
 import './stylesheets/index.css';
 import 'nexious-library/@index.css';
 import App from './pages/Landing.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <BrowserRouter>
+    <ScrollToTop />
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 );
