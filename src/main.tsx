9099b4a8929d6b0a9270f10a5f4af6385463edb8
carlos-hereee <97hernandez.c@gmail.com>
2025-08-27 02:34:06 -0500

    remove strict

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,12 @@ import RoutesRouter from "./utils/router/RoutesRouter.tsx";
 import "nexious-library/@index.css";
 import "./stylesheets/index.css";
 import ScrollToTop from "@utils/router/ScrollToTop.tsx";
-import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter future={{ v7_startTransition: true }}>
-      <ScrollToTop />
-      <App>
-        <RoutesRouter />
-      </App>
-    </BrowserRouter>
-    ,
-  </StrictMode>,
+  <BrowserRouter future={{ v7_startTransition: true }}>
+    <ScrollToTop />
+    <App>
+      <RoutesRouter />
+    </App>
+  </BrowserRouter>,
 );
